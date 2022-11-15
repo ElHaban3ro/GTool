1ce423387b5de80d54308ee0ba9be99d529f927b
ElHaban3ro <Ferdhaban@gmail.com>
2022-11-14 16:37:26 -0800

    (v1.2) Target construc with avatar ready

--- src/generate_target.py
+++ src/generate_target.py
@@ -1,10 +1,13 @@
+import os
 import requests
+from PIL import Image, ImageDraw
 
 
 
+avatar_ubi = (60, 45)
 
 def get_user_info(apikey, user, debug = True):
-    req = requests.get(f'https://api.github.com/users/{user}', headers={'Accept': 'application/vnd.github+json'})
+    req = requests.get(f'https://api.github.com/users/{user}', headers={'Accept': 'application/vnd.github+json', 'Authorization': apikey})
 
     if req.status_code == 404:
         if debug:
@@ -16,7 +19,8 @@ def get_user_info(apikey, user, debug = True):
 
     else:
         raw_userdata = req.json()
-        req_repos = requests.get(f"{raw_userdata['repos_url']}?sort=update", headers={'Accept': 'application/vnd.github+json'})
+
+        req_repos = requests.get(f"{raw_userdata['repos_url']}?sort=update", headers={'Accept': 'application/vnd.github+json', 'Authorization': apikey})
         repos = req_repos.json()
 
         recent_repos = []
@@ -26,7 +30,48 @@ def get_user_info(apikey, user, debug = True):
 
 
 
-        user_data = {'icon': raw_userdata['avatar_url'], 'repos_count': raw_userdata['public_repos'], 'followers': raw_userdata['followers'], 'following': raw_userdata['following'], 'recent': recent_repos}
+        user_data = {
+            'icon': raw_userdata['avatar_url'],
+            'repos_count': raw_userdata['public_repos'], 
+            'followers': raw_userdata['followers'], 
+            'following': raw_userdata['following'], 
+            'recent': recent_repos
+        }
+
+        try:
+            os.mkdir(f'./users/{user}')
+        except:
+            pass
+        
+
+
+        avatar_route = f'./users/{user}/avatar-{user}.png'
+        avatar_download = requests.get(user_data['icon']).content 
+        with open(avatar_route, 'wb+') as image_download:
+            image_download.write(avatar_download)
+
+
+        image_base = Image.open('./src/base/target.png')
+        lienzo = Image.new('RGBA', image_base.size, (0, 0, 0, 0))
+
+        image_avatar = Image.open(avatar_route)
+
+
+        lienzo_avatar = Image.new('RGBA', image_avatar.size, 0)
+        mask_avatar = Image.new('L', image_avatar.size, 0)
+        draw = ImageDraw.Draw(mask_avatar)
+
+        
+        circle = draw.ellipse((0, 0, image_avatar.size[0], image_avatar.size[1]), fill = 255) # Imagen redonda!!
+        image_avatar = Image.composite(image_avatar, lienzo_avatar, mask_avatar).resize((600, 600))
+        lienzo.paste(image_avatar,  (60, 45))
+
+        f_target = Image.alpha_composite(image_base, lienzo)
+
+
+
+        f_target.save(f'./users/{user}/target.png')
+
         
 
 
@@ -37,3 +82,8 @@ def get_user_info(apikey, user, debug = True):
 
 
 
+with open('./config/config.txt', 'r+') as apikey:
+    key = apikey.readlines()[0]
+
+    result = get_user_info(key, 'ElHaban3ro')
+    print(result)
